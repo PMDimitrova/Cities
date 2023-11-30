faba896eaa2a1cfe3c4e074160301bf9dc928ac8
PMDimitrova <palmina.dimitrova@gmail.com>
2023-11-30 09:07:51 +0200

    Adding ReadMe; Final Commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Firebase Realtime DB - https://cities-819f6-default-rtdb.europe-west1.firebasedatabase.app/
+Hello!
+
+This is a very very simple React app, which has only one purpose ->
+to fetch data for some cities and display it in a table.
+
+If you want to see it in action:
+
+1. Clone the repository
+2. Instal all needed dependencies with 'npm instal'
+3. Run the project with 'npm start'
+
+Firebase Realtime DB - 'https://cities-819f6-default-rtdb.europe-west1.firebasedatabase.app/.json'
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,18 +1,18 @@
 import { useStoreMe } from 'store-me';
 
-import cities from '../_constants/cities.json';
 import TableHeader from './TableHeader';
 import TableRow from './TableRow';
 
 const Table = () => {
   const { citiesData, hasLoadedData } = useStoreMe('citiesData', 'hasLoadedData');
+  const countOfCities = hasLoadedData ? citiesData.length : 0;
 
   return (
     <>
       <TableHeader />
       {hasLoadedData &&
         citiesData.map((city, i) => (
-          <TableRow city={city} variant={i} key={city.name} isLastRow={i + 1 === cities.length} />
+          <TableRow city={city} variant={i} key={city.name} isLastRow={i + 1 === countOfCities} />
         ))}
     </>
   );
